7131f17d4b9a74ba49a3e3aa9be8ddef4a3f5def
Barbara Kumor <b.kumor@yahoo.com>
2020-08-05 18:47:48 +0200

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <button>Heroes & Villians</button>
+            <button>Worlds</button>
+        </nav>
+    )
+}
+
+export default Navbar;
