e82768238a018c41d77807d58d29e1de6069b579
Barbara Kumor <b.kumor@yahoo.com>
2020-08-05 18:31:14 +0200

    structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-
+      <h1>Your Star Wars Hub</h1>
+      <div className="content">
+        
+      </div>
     </div>
   );
 }
