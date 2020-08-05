77cc5db78367aaec9af497dba413dc2effc5621b
Barbara Kumor <b.kumor@yahoo.com>
2020-08-05 19:25:35 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   const [content, setContent] = useState('heroes');
   return (
     <div className="App">
-      <h1>Your Star Wars Hub</h1>
+      <h1 className="title">Your Star Wars Hub</h1>
       <Navbar setContent={setContent}/>
-      <div className="content">
+      <div className="content container">
         {content === 'heroes' ? <Heroes /> : <Worlds />}
       </div>
     </div>
--- src/components/Worlds.js
+++ src/components/Worlds.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Worlds = () => {
     return (
         <div>
-            worlds
+            <h2>Explore the worlds</h2>
         </div>
     )
 }
