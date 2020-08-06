59db75bd49135d3989d1a18ded4fb45fa99be331
Barbara Kumor <b.kumor@yahoo.com>
2020-08-06 16:23:19 +0200

    implement useQuery

---
+++ src/components/World.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const World = ({world}) => {
+    return (
+        <div className="card">
+            <h3>{world.name}</h3>
+            <p>Description: {world.terrain}</p>
+            <p>Climate: {world.climate}</p>
+            <p>Population: {world.population}</p>
+        </div>
+    )
+}
+
+export default World
