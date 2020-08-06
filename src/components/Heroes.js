b6265fbbe3d00c8fbbd645c0501f3f831d4efc60
Barbara Kumor <b.kumor@yahoo.com>
2020-08-06 17:03:07 +0200

    styling

--- src/components/Heroes.js
+++ src/components/Heroes.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import {useQuery} from 'react-query';
+import axios from 'axios';
+import Hero from './Hero';
+
+const fetchHeroes = async () => {
+    const HEROES_URL = "https://swapi.dev/api/people";
+    const res = await axios.get(HEROES_URL);
+    return res.data;
+}
 
 const Heroes = () => {
+    const {data, status} = useQuery('heroes', fetchHeroes);
+    console.log(data);
     return (
         <div>
-            <h2>Meet your heroes & villians</h2>
+            <h2 className="subtitle">Meet your heroes & villians</h2>
+            {status === 'loading' && <div className="status-msg">Loading...</div>}
+            {status === 'error' && <div className="status-msg">Ooops! Something didn't go according to the plan.</div>}
+            {status === 'success' && (
+                <div className="data">
+                    {data.results.map(hero => <Hero hero={hero} key={hero.name}/>)}
+                </div>
+            )}
         </div>
     )
 }
