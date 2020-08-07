5c115a2d6bdd815283696b8e9d41917da5a02358
Basia <b.kumor@yahoo.com>
2020-08-07 19:39:25 +0200

    pagination

--- src/components/Worlds.js
+++ src/components/Worlds.js
@@ -17,9 +17,21 @@ const Worlds = () => {
         onSuccess: () => console.log('success')
     });
 
+    const handleNextPage = () => {
+        if (page < 6) setPage(prevPage => prevPage += 1);
+    }
+
+    const handlePreviousPage = () => {
+        if (page > 1) setPage(prevPage => prevPage -= 1);
+    }
+
     return (
         <div>
             <h2 className="subtitle">Explore the worlds</h2>
+            <div className="btns">
+                <button className={page === 1 ? "page-btn disabled" : "page-btn"} onClick={handlePreviousPage}>Previous</button>
+                <button className={page === 6 ? "page-btn disabled" : "page-btn"} onClick={handleNextPage}>Next</button>
+            </div>
             {status === 'loading' && <div className="status-msg">Loading...</div>}
             {status === 'error' && <div className="status-msg">Ooops! Something didn't go according to the plan.</div>}
             {status === 'success' && (
