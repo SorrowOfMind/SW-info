46acf3d90592f947e33adfbb955c3fde1068077a
Basia <b.kumor@yahoo.com>
2020-08-08 10:27:15 +0200

    added usePaginatedQuery

--- src/components/Heroes.js
+++ src/components/Heroes.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {useQuery} from 'react-query';
+import {usePaginatedQuery} from 'react-query';
 import axios from 'axios';
 import Hero from './Hero';
 
@@ -11,31 +11,27 @@ const fetchHeroes = async (key, page) => {
 
 const Heroes = () => {
     const [page, setPage] = useState(1);
-    const {data, status} = useQuery(['heroes', page], fetchHeroes, {
+    const {resolvedData, latestData, status} = usePaginatedQuery(['heroes', page], fetchHeroes, {
         staleTime: 10000,
         cacheTime: 60000
     });
     
-    const handleNextPage = () => {
-        if (page < 6) setPage(prevPage => prevPage += 1);
-    }
-
-    const handlePreviousPage = () => {
-        if (page > 1) setPage(prevPage => prevPage -= 1);
-    }
+    const handlePreviousPage = () => setPage(prevPage => Math.max(prevPage -= 1, 1));
+    const handleNextPage = () => setPage(prevPage => (!latestData || !latestData.next ? prevPage : prevPage += 1));
 
     return (
         <div>
             <h2 className="subtitle">Meet your heroes & villians</h2>
             <div className="btns">
-                <button className={page === 1 ? "page-btn disabled" : "page-btn"} onClick={handlePreviousPage}>Previous</button>
-                <button className={page === 6 ? "page-btn disabled" : "page-btn"} onClick={handleNextPage}>Next</button>
+                <button className={page === 1 ? "page-btn disabled" : "page-btn"} onClick={handlePreviousPage} disabled={page === 1}>Previous</button>
+                <span className="page-num">{page}</span>
+                <button className={page === 6 ? "page-btn disabled" : "page-btn"} onClick={handleNextPage} disabled={!latestData || !latestData.next}>Next</button>
             </div>
             {status === 'loading' && <div className="status-msg">Loading...</div>}
             {status === 'error' && <div className="status-msg">Ooops! Something didn't go according to the plan.</div>}
             {status === 'success' && (
                 <div className="data">
-                    {data.results.map(hero => <Hero hero={hero} key={hero.name}/>)}
+                    {resolvedData.results.map(hero => <Hero hero={hero} key={hero.name}/>)}
                 </div>
             )}
         </div>
